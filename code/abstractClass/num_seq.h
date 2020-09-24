1187d30e865483e268b4c7d46acef608a5c6e38a
Andrew5c <andrew_qs@163.com>
2020-09-24 10:57:19 +0800

    modify files in LINUX

---
+++ code/abstractClass/num_seq.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: num_seq.h
+	> Author: Andrew. Q 
+	> Describe: 
+	> Created Time: 2020年09月24日 星期四 10时40分54秒
+ ************************************************************************/
+
+#ifndef _NUM_SEQ_H
+#define _NUM_SEQ_H
+
+// 为所有数列定义一个抽象类
+class num_seq{
+    public:
+    
+    virtual ~num_seq() {};
+
+
+    protected:
+
+
+}
+
+#endif
