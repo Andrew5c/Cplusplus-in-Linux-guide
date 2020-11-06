fe7b8ec6ab420604866b977dabb3f754dd802622
Andrew5c <andrew_qs@163.com>
2020-11-06 10:31:06 +0800

    modify files in LINUX

--- code/DSAA/make2dArray.cpp
+++ code/DSAA/make2dArray.cpp
@@ -6,16 +6,41 @@
  ************************************************************************/
 
 #include<iostream>
+#include<exception>
+#include<vector>
 using namespace std;
 
+// 为二维数组动态分配存储空间
+template<typename T>
+void make2dArray(T** &x, int numOfRow, int numOfClo) {
+    // 创建行指针
+    x = new T* [numOfRow];
+    // 为每一行分配存储空间
+    for(int i=0; i<numOfClo; i++){
+        x[i] = new T [numOfClo];
+    }
+}
 
-template<typenam T>
-T make2dArray() {
-    
+template<typename T>
+void delete2dArray(T** &x, int numOfRow) {
 
 }
 
-
 int main() {
+    float **p = NULL;
+
+    try{
+        make2dArray(p, 3, 3);
+    }catch(bad_alloc){
+        cerr << "out of memory" << endl;
+        exit(1);
+    }
+    // 向这个3x3的矩阵中填充数据
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            p[i][j] = i+1;
+        }
+    }
+    cout << "p[1][1] = " << p[1][1] << endl;
     return 0;
 }
