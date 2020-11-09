cc77aec04a896c94bd38c438d1cab39a8399f486
Andrew5c <andrew_qs@163.com>
2020-11-09 10:01:05 +0800

    modify files in LINUX

--- code/DSAA/make2dArray.cpp
+++ code/DSAA/make2dArray.cpp
@@ -8,12 +8,14 @@
 #include<iostream>
 #include<exception>
 #include<vector>
+#include<cstdlib>
 using namespace std;
 
 // 为二维数组动态分配存储空间
 template<typename T>
 void make2dArray(T** &x, int numOfRow, int numOfClo) {
     // 创建行指针
+    // 利用数组指针保存后面二维数组中每一行的头指针
     x = new T* [numOfRow];
     // 为每一行分配存储空间
     for(int i=0; i<numOfClo; i++){
@@ -21,9 +23,15 @@ void make2dArray(T** &x, int numOfRow, int numOfClo) {
     }
 }
 
+// 删除二维数组
 template<typename T>
 void delete2dArray(T** &x, int numOfRow) {
-
+    // 删除行数组空间
+    for(int i=0; i<numOfRow; i++)
+        delete [] x[i];
+    // 删除行指针
+    delete [] x;
+    x = NULL;
 }
 
 int main() {
@@ -42,5 +50,8 @@ int main() {
         }
     }
     cout << "p[1][1] = " << p[1][1] << endl;
+    delete2dArray(p, 3);
+    cout << "the 2-d array has been delete!" << endl;
+
     return 0;
 }
